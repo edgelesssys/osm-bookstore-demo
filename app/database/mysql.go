7d0244cff111280edd29f1e6cbad5d33f0840871
Nils Hanke <nils.hanke@rub.de>
2021-09-11 07:17:23 +0000

    wip: Initial EdgelessDB setup

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -1,24 +1,51 @@
 package database
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
 
+	"github.com/edgelesssys/ego/marble"
+	mysql_driver "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // Refer to docs/example/manifests/apps/mysql.yaml for database setup
 const (
-	dbuser = "root"
-	dbpass = "mypassword"
+	dbuser = "bookwarehouse"
 	dbport = 3306
 	dbname = "booksdemo"
 )
 
 // GetMySQLConnection returns a MySQL connection using default configuration
 func GetMySQLConnection() (*gorm.DB, error) {
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&timeout=20s", dbuser, dbpass, "mysql.bookwarehouse", dbport, dbname)
-	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
+	// Setup TLS for SQL connection
+	pem := []byte(os.Getenv(marble.MarbleEnvironmentRootCA))
+	rootCertPool := x509.NewCertPool()
+	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+		panic("failed to append PEM.")
+	}
+
+	// Get certificate + private key for bookwarehouse
+	cert, err := tls.X509KeyPair([]byte(os.Getenv("SQL_CERT")), []byte(os.Getenv("SQL_KEY")))
+	if err != nil {
+		panic("failed to create X509KeyPair.")
+	}
+
+	// Register SQL TLS config in the MySQL driver
+	mysql_driver.RegisterTLSConfig("edgelessdb", &tls.Config{
+		RootCAs:      rootCertPool,
+		Certificates: []tls.Certificate{cert},
+	})
+
+	// Create MySQL connection with the above created TLS configuration
+	connStr := fmt.Sprintf("%s@tcp(%s:%d)/%s?tls=%s&charset=utf8mb4&timeout=20s", dbuser, "127.0.0.1", dbport, dbname, "edgelessdb")
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DriverName: "mysql",
+		DSN:        connStr,
+	}), &gorm.Config{})
 
 	return db, err
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/edgelesssys/osm-bookstore-demo
 go 1.16
 
 require (
+	github.com/edgelesssys/ego v0.3.2
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/openservicemesh/osm v0.9.2
 	gorm.io/driver/mysql v1.1.2
-	gorm.io/gorm v1.21.14
+	gorm.io/gorm v1.21.15
 )
 
 replace (
